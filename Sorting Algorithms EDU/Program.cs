6fdd2f949df389cb09277e34a9a0c3ae6ca04298
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2018-07-20 12:24:25 +1000

    Default sort implementation

--- Sorting Algorithms EDU/Program.cs
+++ Sorting Algorithms EDU/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 // Sorting library.
 using Sorting;
 using static Sorting.Helpers;
@@ -10,23 +10,30 @@ namespace Sorting_Algorithms_EDU {
         static void Main(string[] args) {
             string ln = new string('=', 10);
             for (int i = 0; i < 50; i++) {
-                int[] select = Helpers.GenerateValues(50000);
+                int[] select = GenerateValues(50000);
                 int[] insert = (int[])select.Clone();
-                int[] quick = (int[])select.Clone();
+                int[] quick = (int[])insert.Clone();
+                int[] orderBy = (int[])quick.Clone();
 
                 Console.WriteLine($"{ln} Generated values for {i} {ln}");
 
-                long selection =
-                    TimeMethod(() => SortingAlgorithms.SelectionSort(ref select));
-                Console.WriteLine($"Selection sort took {selection}ms.");
+                long ordersort =
+                    TimeMethod(() => orderBy = orderBy.OrderBy(x => x).ToArray());
+                Console.WriteLine($"OrderBy sort took {ordersort}ms.");
+
+                long quicksort =
+                    TimeMethod(() => SortingAlgorithms.Quicksort(ref quick));
+                Console.WriteLine($"Quicksort took {quicksort}ms.");
 
                 long insertion =
                     TimeMethod(() => SortingAlgorithms.InsertionSort(ref insert));
                 Console.WriteLine($"Insertion sort took {insertion}ms.");
 
-                long quicksort =
-                    TimeMethod(() => SortingAlgorithms.Quicksort(ref insert));
-                Console.WriteLine($"Quicksort took {quicksort}ms.");
+                long selection =
+                    TimeMethod(() => SortingAlgorithms.SelectionSort(ref select));
+                Console.WriteLine($"Selection sort took {selection}ms.");
+
+
             }
             
             Console.ReadKey();
